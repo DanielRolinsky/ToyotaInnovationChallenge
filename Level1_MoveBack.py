1ebc4ba78c5bf1f6aac337175e9da7637369abd5
irrationart <145728917+irrationart@users.noreply.github.com>
2024-05-25 16:50:12 -0400

    Fix Level 1

--- Level1_MoveBack.py
+++ Level1_MoveBack.py
@@ -24,7 +24,7 @@ print("running main")
 #add starter functions here
 robot.start_keyboard_control()
 
-CONST_reverseVelocity = 0.25
+CONST_reverseVelocity = -0.25
 CONST_boundary = 0.3
 
 #rclpy,spin_once is a function that updates the ros topics once
@@ -44,11 +44,11 @@ try:
             print(">>>minDis & minAngle is: " + str(minDistance) + ", " +str(minAngle))
             if minDistance != -1 and minDistance <= CONST_boundary:
                 robot.stop_keyboard_control()
-                robot.set_cmd_vel(-CONST_reverseVelocity, 0, 1)
+                robot.set_cmd_vel(CONST_reverseVelocity, 0, 0.5)
                 robot.set_cmd_vel(0, 0, 1)
                 minDistance, minAngle = robot.detect_obstacle(robot.last_scan_msg.ranges)
                 disToTravel = CONST_boundary + 0.05 - minDistance
-                robot.set_cmd_vel(CONST_reverseVelocity, 0, disToTravel/CONST_reverseVelocity + 1) #Forward is backwards b/c lidar is on the back
+                robot.set_cmd_vel(CONST_reverseVelocity, 0, disToTravel/CONST_reverseVelocity + 0.5) #Forward is backwards b/c lidar is on the back
                 robot.start_keyboard_control()
 
 
