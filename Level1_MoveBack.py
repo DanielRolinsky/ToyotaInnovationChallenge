0756799174bbdf0a5855bdce12542879b89ce029
TonyH <tony.hoalga@gmail.com>
2024-05-26 12:08:29 -0400

    Complete Level1

--- Level1_MoveBack.py
+++ Level1_MoveBack.py
@@ -25,7 +25,7 @@ print("running main")
 robot.start_keyboard_control()
 
 CONST_reverseVelocity = -0.25
-CONST_boundary = 0.3
+CONST_boundary = 0.4
 
 #rclpy,spin_once is a function that updates the ros topics once
 rclpy.spin_once(robot, timeout_sec=0.1)
@@ -39,22 +39,30 @@ try:
         rclpy.spin_once(robot, timeout_sec=0.1)
 
         #Add looping functionality here
+        ##if robot.last_scan_msg:
         if robot.last_scan_msg:
             minDistance, minAngle = robot.detect_obstacle(robot.last_scan_msg.ranges)
-            print(">>>minDis & minAngle is: " + str(minDistance) + ", " +str(minAngle))
-            if minDistance != -1 and minDistance <= CONST_boundary:
-                robot.stop_keyboard_control()
-                robot.set_cmd_vel(CONST_reverseVelocity, 0, 0.5)
-                robot.set_cmd_vel(0, 0, 1)
-                minDistance, minAngle = robot.detect_obstacle(robot.last_scan_msg.ranges)
-                disToTravel = CONST_boundary + 0.05 - minDistance
-                robot.set_cmd_vel(CONST_reverseVelocity, 0, disToTravel/CONST_reverseVelocity + 0.5) #Forward is backwards b/c lidar is on the back
-                robot.start_keyboard_control()
-
-
+        else:
+            minDistance, minAngle = -1.0, -1.0
+        #print(">>>minDis = " + str(minDistance) + " >>>minAng = " +str(minAngle))
         
+        #Seeing if robot is 0.3m from an object, reversing from that directions
+        if minDistance != -1 and minDistance <= CONST_boundary:
+            robot.stop_keyboard_control()
+            robot.set_cmd_vel(CONST_reverseVelocity * 2, 0, 0.5)
+            robot.set_cmd_vel(0, 0, 1.0)
+            minDistance, minAngle = robot.detect_obstacle(robot.last_scan_msg.ranges)
+            print(">>>>>>>>>>>>>>>>" + str(minDistance))
+            if minDistance != -1:
+                print("start backup")
+                disToTravel = CONST_boundary + 0.075 - minDistance
+                robot.set_cmd_vel(CONST_reverseVelocity, 0, disToTravel/math.fabs(CONST_reverseVelocity) + 0.5) #Forward is backwards b/c lidar is on the back
+                print("finish backup")
+            robot.start_keyboard_control()
+
 except KeyboardInterrupt:
     print("keyboard interrupt receieved.Stopping...")
+
 finally:
     #when exiting program, run the kill processes
     #add functionality to ending processes here
